baec4ed65b40d518fda21ed437bd515f188eeb36
Singgih Budi Purnadi <57170834+sindev08@users.noreply.github.com>
2023-05-27 12:44:49 +0700

    console.log providers

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -38,6 +38,7 @@ export default function Login({ providers }: any) {
 	if (session) {
 		router.push("/");
 	}
+	console.log(providers);
 	return (
 		<div className="container flex flex-col items-center justify-center h-screen m-auto">
 			<Head>
